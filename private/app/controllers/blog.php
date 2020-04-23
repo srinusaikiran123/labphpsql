885287a7c3ebc36715f345d1d62c37ed686b6152
srinusaikiran123 <srinusaikiran123@gmail.com>
2020-04-23 19:25:03 +0000

    saikiran

--- private/app/controllers/blog.php
+++ private/app/controllers/blog.php
@@ -2,7 +2,7 @@
 
 class Blog extends Controller
 {
-    function__construct()
+    function __construct()
     {
         parent::__construct();
     
@@ -21,5 +21,37 @@ function Index()
 function Read($postId)
 {
     $this -> model("BlogModel");
-    $post = $this -> BlogModel -> getPostById
+    $post = $this -> BlogModel -> getPostById($postId);
+    $this -> view("blog/header", $post);
+    $this -> view("blog/post", $post);
+    $this -> view("template/footer");
+
 }
+function Create()
+{
+    $is_auth = isset($_SESSION["username"]);
+    if(!$is_auth)
+    {
+       header("location: /blog");
+       return;
+    }
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $title = $_POST["title"];
+        $content = $_POST["content"];
+        $author = $_POST["author"];
+         $this -> model("BlogModel");
+         $slug = $this -> BlogModel -> createPost($title, $author, $content);
+         header("location: /blog/read/" .$slug);
+    }
+    
+    else
+    {
+        $this -> view("template/header");
+        $this -> view("blog/create");
+        $this -> view("template/footer");
+
+}
+}
+}
+?>
